92c2219ed2370643e3215bbd0f207527acf965c2
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-04-14 18:33:30 +0100

    Project Init

---
+++ src/main/java/com/projectpi/cloudwaves/CloudWavesApplication.java
@@ -0,0 +1,13 @@
+package com.projectpi.cloudwaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWavesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudWavesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectpi/cloudwaves/CloudWavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectpi.cloudwaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWavesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
