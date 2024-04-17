d8afc5fb20a67e6b2b767ca07d39d80a31a1e9a1
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-04-17 13:32:33 +0100

    init backend

---
+++ back-end/pfeconnect/src/main/java/com/cloudwaves/pfeconnect/PfeconnectApplication.java
@@ -0,0 +1,13 @@
+package com.cloudwaves.pfeconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfeconnectApplication.class, args);
+    }
+
+}
---
+++ back-end/pfeconnect/src/test/java/com/cloudwaves/pfeconnect/PfeconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudwaves.pfeconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
