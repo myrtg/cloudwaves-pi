7f5da719940600bf79c49ceaf8515f21e62deea1
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-04-18 08:42:26 +0100

    init Event form ng

--- front-end/public/src/app/components/events/events.component.ts
+++ front-end/public/src/app/components/events/events.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit{
+  formEvent!:FormGroup
+  ngOnInit(): void {
+    this.formEvent = new FormGroup({
+      date_debut: new FormControl('',[Validators.required]),
+      date_fin: new FormControl('',[Validators.required]),
+      description: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]*$')]),
+      nb_places: new FormControl('',[Validators.required,Validators.pattern('^[1-9]*$'),Validators.min(1)]),
+      titre: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]*$')]),
+      tutor: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]*$')]),
+    })
+  }
 
 }
