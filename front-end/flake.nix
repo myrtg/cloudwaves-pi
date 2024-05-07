bbc69f3ab30ff4be983e890780b8d44f673803d1
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-05-07 17:23:29 +0100

    flake.nix

---
+++ front-end/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix Shell for Node js development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs;[
+          nodejs
+          curl
+          wget
+          jq
+        ];
+      };
+    }
+    );
+}
